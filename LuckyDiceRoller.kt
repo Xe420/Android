975420f0f8e2e9eb888581cccbbcf24420ab834a
Elivelton Moraes <64163098+Xe420@users.noreply.github.com>
2021-02-02 20:10:39 -0300

    Create

---
+++ LuckyDiceRoller.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val myFirstDice = Dice(6)
+    val diceRoll = myFirstDice.roll()
+    println("Your ${myFirstDice.numSides} sided dice rolled ${diceRoll}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
