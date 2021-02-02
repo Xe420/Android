51ad3d05d4e703c26d1f54db1cad9b6c2da5f1da
Elivelton Moraes <64163098+Xe420@users.noreply.github.com>
2021-02-02 20:36:54 -0300

    Conditional  -  when statement
    There is no difference in the output, but your code is much more compact and easier to read. This is a powerful tool for writing interesting programs!

--- LuckyDiceRoller.kt
+++ LuckyDiceRoller.kt
@@ -1,24 +1,16 @@
 fun main() {
     val myFirstDice = Dice(6)
     val rollResult = myFirstDice.roll()
-    val luckyNumber = 1
-    println("Your ${myFirstDice.numSides} sided dice rolled ${rollResult}!")
+    val luckyNumber = 3
 
-    
-    if (rollResult == luckyNumber) {
-        println("You win!")
-    } else if (rollResult == 1) {
-        println("So sorry! You rolled a 1. Try again!")
-    } else if (rollResult == 2) {
-        println("Sadly, you rolled a 2. Try again!")
-    } else if (rollResult == 3) {
-        println("Unfortunately, you rolled a 3. Try again!")
-    } else if (rollResult == 4) {
-        println("No luck! You rolled a 4. Try again!")
-    } else if (rollResult == 5) {
-        println("Don't cry! You rolled a 5. Try again!")
-    } else {
-        println("Apologies! you rolled a 6. Try again!")
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        4 -> println("No luck! You rolled a 4. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! you rolled a 6. Try again!")
     }
 }
 
